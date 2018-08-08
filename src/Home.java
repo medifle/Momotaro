062aef69068e6e4bd63b94cdee318863f82f2527
medifle <13296643+medifle@users.noreply.github.com>
2018-08-08 19:53:43 -0400

    add Home, fix Player

---
+++ src/Home.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Home extends Location {
+
+    public Home(Position p, String description, List<Player> people, List<Peach> peaches) {
+        super(p, description, people, peaches);
+    }
+
+    
+
+}
