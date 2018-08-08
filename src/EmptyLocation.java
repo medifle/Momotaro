f2cc0e99e57d9942eaa80d684e8a8dc15ae6cfe1
medifle <13296643+medifle@users.noreply.github.com>
2018-08-08 00:12:54 -0400

    add starter code

---
+++ src/EmptyLocation.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class EmptyLocation extends Location {
+    // Constructs an EmptyLocation
+    public EmptyLocation(Position p, String description) {
+        super(p, description, new ArrayList<Player>(), new ArrayList<Peach>());
+    }
+
+    /**
+     * getter for a Peach
+     */
+    public Peach getPeach() {
+        return null;
+    }
+
+
+    /**
+     * remove a player from a room
+     */
+    public void remove(Player p) {
+        peopleAtLocation.remove(p);
+    }
+}
+  
+    
