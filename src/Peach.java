bca850e5d9c1c0522905908fb5afd9088bcf8e15
medifle <13296643+medifle@users.noreply.github.com>
2018-08-08 00:22:55 -0400

    reformat

--- src/Peach.java
+++ src/Peach.java
@@ -1,24 +1,29 @@
-public class Peach implements Comparable<Peach>{
-  protected int     ripeness;
-  protected boolean bad;
+public class Peach implements Comparable<Peach> {
+    protected int ripeness;
+    protected boolean bad;
 
-  public Peach(int ripeness, boolean bad){
-    this.ripeness = ripeness;
-    this.bad = bad;
-  }
-  
-  public Peach(int ripeness){
-    this(ripeness, false);
-  }
-  
-  public int getRipeness(){ return ripeness; }
-  
-  /** ages a peach in some way */
-  public void age(){}
-  
-  @Override 
-  public int compareTo(Peach other){
-    return 0;
-  }
-  
+    public Peach(int ripeness, boolean bad) {
+        this.ripeness = ripeness;
+        this.bad = bad;
+    }
+
+    public Peach(int ripeness) {
+        this(ripeness, false);
+    }
+
+    public int getRipeness() {
+        return ripeness;
+    }
+
+    /**
+     * ages a peach in some way
+     */
+    public void age() {
+    }
+
+    @Override
+    public int compareTo(Peach other) {
+        return 0;
+    }
+
 }
